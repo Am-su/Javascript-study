0b8b55ff42e5dcc6d3e56ff3312496117a2725a2
An-MinSu <dmdkrgkrrydp@naver.com>
2022-02-09 21:09:33 +0900

    함수분할

--- 인프런/기초-2/js/ex2.js
+++ 인프런/기초-2/js/ex2.js
@@ -1,18 +1,33 @@
+var $earth = null;
+
 $(document).ready(function(){
-  var $earth = $("#earth");
-  
+  initialize();
+  getXY();
+},5000)
+
+//초기화
+function initialize(){
+  $earth = $("#earth");
+}
+
+//clickevent
+function getXY(){
   $("#btnStart").click(function(){
     var x = parseInt($("#txtX").val());
     var y = parseInt($("#txtY").val());
-
-    if((x>=0 && x<=500) && (y>=0 && y<=400)){
-      $earth.css({
-        left:x,
-        top:y
-      })
-    }
-    else{
-      alert("입력된 값이 범위를 벗어났습니다.");
-    }
+    moveEarth(x,y);
   })
-})
+}
+
+//move img
+function moveEarth(x,y){
+  if((x>=0 && x<=500) && (y>=0 && y<=400)){
+    $earth.css({
+      left:x,
+      top:y
+    })
+  }
+  else{
+    alert("입력된 값이 범위를 벗어났습니다.");
+  }
+}
