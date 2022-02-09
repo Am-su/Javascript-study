07c933577baa035b571d71b96b8a51730dd7b1d5
An-MinSu <dmdkrgkrrydp@naver.com>
2022-02-09 21:38:24 +0900

    함수 분리

--- 인프런/기초-2/js/ex3.js
+++ 인프런/기초-2/js/ex3.js
@@ -1,19 +1,31 @@
-$(document).ready(function(){
-  //배열로 입력받음
-  var $images = $("#container1 img");
+var $images = null;
 
+$(document).ready(function() {
+  init();
+  initEvent();
+});
+
+function init(){
+  $images = $("#container1 img");
+}
+
+function initEvent(){
   $("#btnStart").click(function(){
-    var length = $images.length;
-
-    for(var i=0;i<length;i++){
-      var $img = $images.eq(i);
-      var x = 100+((i%3) * 200);
-      var y = 100+(parseInt(i/3) * 200);
-
-      $img.css({
-        left:x,
-        top:y
-      })
-    }
+    showImage();
   })
-})
+}
+
+function showImage(){
+  var length = $images.length;
+
+  for(var i=0;i<length;i++){
+    var $img = $images.eq(i);
+    var x = 100+((i%3) * 200);
+    var y = 100+(parseInt(i/3) * 200);
+
+    $img.css({
+      left:x,
+      top:y
+    })
+  }
+}
