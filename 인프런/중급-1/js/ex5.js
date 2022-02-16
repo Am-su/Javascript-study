03423c3978a74decd624a1b1fc73d5326c9aa1e2
An-MinSu <dmdkrgkrrydp@naver.com>
2022-02-17 00:21:41 +0900

    add deleteButton

--- 인프런/중급-1/js/ex5.js
+++ 인프런/중급-1/js/ex5.js
@@ -1,10 +1,28 @@
 function newRegister(){
   var newP = document.createElement("p");
   var userName = document.querySelector("#userName");
-  var text = document.createTextNode(userName.value);
-  newP.appendChild(text);
+  var newtext = document.createTextNode(userName.value);
+  newP.appendChild(newtext);
+
+  var delBtn = document.createElement("span");
+  var delText = document.createTextNode("X");
+  delBtn.setAttribute("class","del");
+  delBtn.appendChild(delText);
+  newP.appendChild(delBtn);
+
+
 
   var nameList = document.querySelector("#nameList");
-  nameList.appendChild(newP);
+  nameList.insertBefore(newP,nameList.childNodes[0]);
   userName.value = "";
+
+  var removeBtns = document.querySelectorAll(".del");
+
+  for(var i=0;i<removeBtns.length;i++){
+    removeBtns[i].addEventListener("click",function(){
+      if(this.parentNode.parentNode){
+        this.parentNode.parentNode.removeChild(this.parentNode);
+      }
+    })
+  }
 }
